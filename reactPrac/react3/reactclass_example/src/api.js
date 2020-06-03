3bae4e043ada3f5fa7927b0ea5df9282d85e6527
Syed Rafi Naqvi <sd.rafi1819@gmail.com>
2020-06-03 17:43:13 +0530

    added the click event

--- reactPrac/react3/reactclass_example/src/api.js
+++ reactPrac/react3/reactclass_example/src/api.js
@@ -6,17 +6,27 @@ class Api extends Component {
         this.state = {
             userId: '',
             title: '',
+            count: 1
         }
+        this.handelClick = this.handelClick.bind(this)
+        // this.handelTitle = this.handelTitle.bind(this)
     }
-    componentDidMount() {
-        fetch('https://jsonplaceholder.typicode.com/todos/1')
-            .then(response => response.json())
-            .then(json => console.log(this.setState({ userId: json.userId, title: json.title })))
+    // handelTitle() {
+    //     fetch(`https://jsonplaceholder.typicode.com/todos/${this.state.count}`)
+    //         .then(response => response.json())
+    //         .then(json => console.log(this.setState({ userId: json.userId, title: json.title })))
+    // }
 
+    handelClick() {
+        fetch(`https://jsonplaceholder.typicode.com/todos/${this.state.count}`)
+            .then(response => response.json())
+            .then(json => console.log(this.setState({ userId: json.id, title: json.title })))
+        this.setState({ count: this.state.count + 1 })
     }
     render() {
         return (
-            <p> {this.state.userId} : { this.state.title}</p>
+            <p onClick={this.handelClick}> {this.state.userId} : { this.state.title}</p>
+
         )
     }
 }
