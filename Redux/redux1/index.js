58961630c62b15e2f809a73c04401db64425c4d1
Syed Rafi Naqvi <sd.rafi1819@gmail.com>
2020-08-10 22:29:28 +0530

    Start this again

--- Redux/redux1/index.js
+++ Redux/redux1/index.js
@@ -2,6 +2,7 @@ const redux = require('redux')
 
 
 const BUY_CAKE = 'BUY_CAKE'
+const BUY_ICE_CREAM = 'BUY_ICE_CREAM'
 
 // Action Creator
 function buyCake() {
@@ -12,11 +13,22 @@ function buyCake() {
     }
 }
 
+function buyIceCream() {
+    //creating a action - to make a change in store
+    return {
+        type: BUY_ICE_CREAM,
+        info: 'helps to buy ice cream'
+    }
+}
+
 // intialState of whole app
-const intialState = {
+const cakeIntialState = {
     numberOfCakes: 10
 }
 
+const iceCreamIntialState = {
+    numberOfIceCream: 30
+}
 
 // Reducer - takes previousState and action gives new state
 const reducer = (state = intialState, action) => {
@@ -28,12 +40,38 @@ const reducer = (state = intialState, action) => {
         default: return state
     }
 }
-
+const cakeReducer = (state = cakeIntialState, action) => {
+    switch (action.type) {
+        case 'BUY_CAKE': return {
+            ...state,
+            numberOfCakes: state.numberOfCakes - 1
+        }
+        default: return state
+    }
+}
+const iceCreamReducer = (state = iceCreamIntialState, action) => {
+    switch (action.type) {
+        case 'BUY_ICE_CREAM': return {
+            ...state,
+            numberOfIceCream: state.numberOfIceCream - 1
+        }
+        default: return state
+    }
+}
 //Store is the state of app in an object, gives state => getState, take cares of Dispatch and subscribe.
-const store = redux.createStore(reducer)
+
+const rootReducer = redux.combineReducers({
+    cakeReducer,
+    iceCreamReducer
+})
+const store = redux.createStore(rootReducer)
 console.log('intialState', store.getState())
 const unsub = store.subscribe(() => console.log('updated state', store.getState()))
 store.dispatch(buyCake())
 store.dispatch(buyCake())
 store.dispatch(buyCake())
+store.dispatch(buyIceCream())
+store.dispatch(buyIceCream())
+store.dispatch(buyIceCream())
+store.dispatch(buyIceCream())
 // unsub()
